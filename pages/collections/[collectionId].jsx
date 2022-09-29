2238bcb6466cd0e09e42806b7b46ac5d94008a8f
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-09-29 23:38:37 +0530

    Navbar updated

---
+++ pages/collections/[collectionId].jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+
+
+const Collection = () => {
+    const router = useRouter();
+    return (
+        <Link href="/">
+            <h2>{router.query.collectionId}</h2>
+        </Link>
+    )
+}
+
+export default Collection
