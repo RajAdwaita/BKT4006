641d42e11b1c3d712e06a43b1c2b70550e57fc02
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-11-16 19:09:11 +0530

    Files Updated

--- BKT4006/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import NFTReducer from '../features/NFT/NFTSlice';
-
-export const store = configureStore({
-  reducer: {
-    NFT:NFTReducer,
-  },
-});
